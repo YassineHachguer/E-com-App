470e0de5afb07a81d3b7e44918d42d832d4c1169
YassineHachguer <yassine.hachguer05@gmail.com>
2025-10-13 15:18:45 +0100

    Implémentation du microservice Billing

---
+++ billing-service/src/main/java/com/example/billingservice/entities/Bill.java
@@ -0,0 +1,30 @@
+package com.example.billingservice.entities;
+
+import com.example.billingservice.model.Customer;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.Collection;
+import java.util.Date;
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class Bill {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private Date billingDate;
+    @OneToMany(mappedBy = "bill")
+    private Collection<ProductItem> productItems;
+    private Long customerID;
+
+    @Transient
+    private Customer customer;
+
+}
---
+++ billing-service/src/main/java/com/example/billingservice/entities/ProductItem.java
@@ -0,0 +1,31 @@
+package com.example.billingservice.entities;
+
+import com.example.billingservice.model.Product;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class ProductItem {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private double quantity;
+    private double price;
+    private long productID;
+    @JsonProperty(access = JsonProperty.Access.WRITE_ONLY)
+    @ManyToOne
+    private Bill bill;
+    @Transient
+    private Product product;
+
+    @Transient
+    private String productName;
+}
