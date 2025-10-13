30ece93a0b7acd2ba337873a5969197f3d232ee8
YassineHachguer <yassine.hachguer05@gmail.com>
2025-10-13 10:55:31 +0100

    Implémentation du microservice Customer

--- customer-service/src/main/java/com/example/customerservice/CustomerServiceApplication.java
+++ customer-service/src/main/java/com/example/customerservice/CustomerServiceApplication.java
@@ -1,13 +1,38 @@
 package com.example.customerservice;
 
+import com.example.customerservice.entities.Customer;
+import com.example.customerservice.repository.CustomerRepository;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.data.rest.core.config.RepositoryRestConfiguration;
 
 @SpringBootApplication
 public class CustomerServiceApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(CustomerServiceApplication.class, args);
+	}
+
+	@Bean
+	CommandLineRunner start(CustomerRepository customerRepository, RepositoryRestConfiguration repositoryRestConfiguration) {
+		repositoryRestConfiguration.exposeIdsFor(Customer.class);
+		return args -> {
+			customerRepository.save(
+					new Customer(null, "yassine", "yassine@gmail.com")
+			);
+			customerRepository.save(
+					new Customer(null, "yahya", "yahya@gmail.com")
+			);
+			customerRepository.save(
+					new Customer(null, "ali", "ali@gmail.com")
+			);
+			customerRepository.findAll().forEach(c ->{
+				System.out.println(c.toString());
+			});
+
+		};
 	}
 
 }
--- customer-service/src/main/java/com/example/customerservice/repository/CustomerRepository.java
+++ customer-service/src/main/java/com/example/customerservice/repository/CustomerRepository.java
@@ -1,4 +1,9 @@
 package com.example.customerservice.repository;
 
-public interface CustomerRepository {
+import com.example.customerservice.entities.Customer;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+
+@RepositoryRestResource
+public interface CustomerRepository extends JpaRepository<Customer, Long> {
 }
