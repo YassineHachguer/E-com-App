f75a01a0aad48ea177a2e91bbd1b5877f69e5293
YassineHachguer <yassine.hachguer05@gmail.com>
2025-10-15 14:11:02 +0100

    test config

--- config-service/src/main/java/com/example/configservice/ConfigServiceApplication.java
+++ config-service/src/main/java/com/example/configservice/ConfigServiceApplication.java
@@ -2,8 +2,11 @@ package com.example.configservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.cloud.config.server.EnableConfigServer;
 
 @SpringBootApplication
+@EnableConfigServer
 public class ConfigServiceApplication {
 
 	public static void main(String[] args) {
