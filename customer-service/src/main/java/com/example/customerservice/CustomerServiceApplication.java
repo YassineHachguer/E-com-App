b0fe2ff98529def45491d5e122501d02e6d0976c
YassineHachguer <yassine.hachguer05@gmail.com>
2025-10-15 14:53:47 +0100

    Test de la config via le controller

--- customer-service/src/main/java/com/example/customerservice/CustomerServiceApplication.java
+++ customer-service/src/main/java/com/example/customerservice/CustomerServiceApplication.java
@@ -1,14 +1,17 @@
 package com.example.customerservice;
 
+import com.example.customerservice.config.CustomerConfigParams;
 import com.example.customerservice.entities.Customer;
 import com.example.customerservice.repository.CustomerRepository;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.data.rest.core.config.RepositoryRestConfiguration;
 
 @SpringBootApplication
+@EnableConfigurationProperties(CustomerConfigParams.class)
 public class CustomerServiceApplication {
 
 	public static void main(String[] args) {
